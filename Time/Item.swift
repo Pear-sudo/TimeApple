c9776599a1662ff02f4b36e76c94f0b200b854fc
adam <adameve1981zero@gmail.com>
2024-07-10 21:10:21 +0800

    Initial Commit

---
+++ Time/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Time
+//
+//  Created by A on 10/07/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
