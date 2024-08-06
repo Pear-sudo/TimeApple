34cb9108dcb58e5a09638a630430dbaf9a41d66c
adam <adameve1981zero@gmail.com>
2024-08-06 16:19:31 +0800

    extract ProjectsHeader

---
+++ Time/TimeView/ProjectsHeader.swift
@@ -0,0 +1,52 @@
+//
+//  ProjectsHeader.swift
+//  Time
+//
+//  Created by A on 06/08/2024.
+//
+
+import SwiftUI
+
+struct ProjectsHeader: View {
+    
+    private var headerProjects: [ProjectItem]
+    
+    private let horizontalSpacing: CGFloat = 8
+    
+    init(headerProjects: [ProjectItem]) {
+        self.headerProjects = headerProjects
+    }
+    
+    var body: some View {
+        ZStack {
+            ProjectHeaderView(project: headerProjects.first!, isDummy: true)
+                .disabled(true)
+                .hidden()
+            GeometryReader { geometry in
+                ScrollView([.horizontal]) {
+                    HStack(spacing: horizontalSpacing) {
+                        ForEach(headerProjects) { project in
+                            ProjectHeaderView(project: project)
+                                .frame(minWidth: calculateActiveProjectViewWidth(geometry.size.width))
+                        }
+                    }
+                }
+                .scrollIndicators(.never, axes: [.horizontal, .vertical]) // do not use hidden; it will show white space on macos, and that's the indicator
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+            }
+        }
+        .listRowBackground(Color.white.opacity(0)) // note that this view is part of the surrounding list
+        .listRowSeparator(.hidden)
+        .shadow(radius: 3)
+    }
+    
+    private func calculateActiveProjectViewWidth(_ width: CGFloat) -> CGFloat {
+        
+        let minWidth: CGFloat = 100
+        let numberOfPeriods = CGFloat(headerProjects.count)
+        let availableWidth = width - (numberOfPeriods - 1) * horizontalSpacing
+        let calculatedWidth = availableWidth / numberOfPeriods
+        
+        return max(minWidth, calculatedWidth)
+    }
+}
