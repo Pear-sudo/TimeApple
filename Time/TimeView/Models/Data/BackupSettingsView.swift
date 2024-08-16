d8c58df4b736bb348601fc319cd287d09f51fd06
adam <adameve1981zero@gmail.com>
2024-08-16 12:53:06 +0800

    make sure the selected folder is readable

--- Time/TimeView/Models/Data/BackupSettingsView.swift
+++ Time/TimeView/Models/Data/BackupSettingsView.swift
@@ -25,7 +25,44 @@ struct BackupSettingsView: View {
             Button("Folder") {
                 fileImporterIsPresented = true
             }
-            Text(String(data: backupURL, encoding: .utf8) ?? "No path")
+            HStack {
+                ShortLayout(anchor: 0) {
+                    Text(urlPath)
+                        .contextMenu {
+                            Button("Copy path") {
+                                let pasteboard = NSPasteboard.general
+                                pasteboard.clearContents()
+                                let result = pasteboard.setString(urlPath, forType: .string)
+                                if result == false {
+                                    logger.error("Cannot set pasteboard")
+                                }
+                            }
+                            Button("Show in Finder") {
+                                let workspace = NSWorkspace.shared
+                                if let url = loadThisBookmark() {
+                                    let result = workspace.selectFile(url.path(), inFileViewerRootedAtPath: "")
+                                    if !result {
+                                        logger.error("Cannot open \(url.path()) in finder")
+                                    }
+                                }
+                            }
+                        }
+                    if checkThisFolderURL() {
+                        Image(systemName: "checkmark.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundStyle(.green)
+                            .help("Folder is accessible")
+                    } else {
+                        Image(systemName: "exclamationmark.triangle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundStyle(.red)
+                            .help("Cannot access this folder")
+                    }
+                }
+            }
+            
         }
         .fileExporter(isPresented: $fileExporterIsPresented, item: exporter, onCompletion: { result in
             if case .success(let url) = result {
@@ -43,15 +80,79 @@ struct BackupSettingsView: View {
         guard let url = urls.first else {
             return
         }
-        if let urlData = url.absoluteString.data(using: .utf8) {
-            backupURL = urlData
+        guard let bookmark = createBookmarkData(url: url) else {
+            return
         }
+        backupURL = bookmark
+    }
+    
+    var urlPath: String {
+        if let path = loadBookmark(bookmarkData: backupURL)?.path(), path != "" {
+            return path
+        }
+        return "No path"
     }
     
     private func createBookmarkData(url: URL) -> Data? {
-        return try? url.bookmarkData(
-            options: .withSecurityScope
-        )
+        var data: Data? = nil
+        useBookmark(url) { url in
+            do {
+                let bookmarkData = try url.bookmarkData(
+                    options: .withSecurityScope
+                )
+                data = bookmarkData
+            } catch {
+                logger.error("Cannot create bookmark from url: \(error)")
+            }
+        }
+        return data
+    }
+    
+    private func loadBookmark(bookmarkData: Data) -> URL? {
+        do {
+            var isStale = false
+            let url = try URL(resolvingBookmarkData: backupURL, options: .withSecurityScope, relativeTo: nil, bookmarkDataIsStale: &isStale)
+            if isStale, let bookmarkData = createBookmarkData(url: url) {
+                backupURL = bookmarkData
+            }
+            return url
+        } catch {
+            logger.error("Cannot load bookmark url: \(error)")
+            return nil
+        }
+    }
+    
+    private func loadThisBookmark() -> URL? {
+        loadBookmark(bookmarkData: backupURL)
+    }
+    
+    private func useBookmark<T>(_ url: URL, to action: (URL) -> T ) -> T? {
+        if url.startAccessingSecurityScopedResource() {
+            defer {
+                url.stopAccessingSecurityScopedResource()
+            }
+            return action(url)
+        } else {
+            logger.error("Call to startAccessingSecurityScopedResource() failed")
+            return nil
+        }
+    }
+    
+    private func checkFolderURL(url: URL) -> Bool {
+        return url.hasDirectoryPath && ((try? url.checkResourceIsReachable()) ?? false)
+    }
+    
+    private func checkThisFolderURL() -> Bool {
+        guard let bookmark = loadBookmark(bookmarkData: backupURL) else {
+            return false
+        }
+        return useBookmark(bookmark, to: { url in
+            checkFolderURL(url: url)
+        }) ?? false
+    }
+    
+    private func countBackupFiles(url: URL) -> Int {
+        return 0
     }
     
     private func onFolderSelectionCancelled() {
